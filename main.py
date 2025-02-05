cc38e90cca170ffa285e09639fb566a7bd28e07d
bros737 <nagaraju212005@gmail.com>
2025-02-05 19:12:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import app1 as ap
 
 org_name="123"
-org_pass="119@J"
+org_pass="android"
 username = st.text_input("Username", placeholder="Enter Username")
 password = st.text_input("Password", type="password", placeholder="Enter Password")
 
